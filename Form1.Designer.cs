8183ac30e3ec20c25ce302f68d76040ffe44e7fb
LucasCruzparedes <lucascruzparedess@gmail.com>
2026-04-04 22:32:13 -0300

    comite inicial

---
+++ Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace CalculadoraIMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.ALTURA = new System.Windows.Forms.Label();
+            this.RESULTADO = new System.Windows.Forms.Label();
+            this.CALCULAR = new System.Windows.Forms.Button();
+            this.LIMPAR = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.PESO = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(87, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 39);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox2.Location = new System.Drawing.Point(3, 181);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(330, 31);
+            this.textBox2.TabIndex = 1;
+            // 
+            // ALTURA
+            // 
+            this.ALTURA.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ALTURA.AutoSize = true;
+            this.ALTURA.Font = new System.Drawing.Font("MingLiU-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ALTURA.Location = new System.Drawing.Point(110, 69);
+            this.ALTURA.Name = "ALTURA";
+            this.ALTURA.Size = new System.Drawing.Size(116, 32);
+            this.ALTURA.TabIndex = 4;
+            this.ALTURA.Text = "ALTURA";
+            // 
+            // RESULTADO
+            // 
+            this.RESULTADO.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.RESULTADO.AutoSize = true;
+            this.RESULTADO.Font = new System.Drawing.Font("MingLiU-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RESULTADO.Location = new System.Drawing.Point(84, 146);
+            this.RESULTADO.Name = "RESULTADO";
+            this.RESULTADO.Size = new System.Drawing.Size(167, 32);
+            this.RESULTADO.TabIndex = 5;
+            this.RESULTADO.Text = "RESULTADO";
+            // 
+            // CALCULAR
+            // 
+            this.CALCULAR.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.CALCULAR.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.CALCULAR.Location = new System.Drawing.Point(68, 218);
+            this.CALCULAR.Name = "CALCULAR";
+            this.CALCULAR.Size = new System.Drawing.Size(200, 76);
+            this.CALCULAR.TabIndex = 6;
+            this.CALCULAR.Text = "CALCULAR";
+            this.CALCULAR.UseVisualStyleBackColor = false;
+            this.CALCULAR.Click += new System.EventHandler(this.CALCULAR_Click);
+            // 
+            // LIMPAR
+            // 
+            this.LIMPAR.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LIMPAR.BackColor = System.Drawing.Color.Red;
+            this.LIMPAR.Location = new System.Drawing.Point(68, 300);
+            this.LIMPAR.Name = "LIMPAR";
+            this.LIMPAR.Size = new System.Drawing.Size(200, 76);
+            this.LIMPAR.TabIndex = 7;
+            this.LIMPAR.Text = "LIMPAR";
+            this.LIMPAR.UseVisualStyleBackColor = false;
+            this.LIMPAR.Click += new System.EventHandler(this.LIMPAR_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(691, 15);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(362, 305);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox3.Location = new System.Drawing.Point(87, 35);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(161, 31);
+            this.textBox3.TabIndex = 2;
+            // 
+            // PESO
+            // 
+            this.PESO.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.PESO.AutoSize = true;
+            this.PESO.Font = new System.Drawing.Font("MingLiU-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PESO.Location = new System.Drawing.Point(127, 0);
+            this.PESO.Name = "PESO";
+            this.PESO.Size = new System.Drawing.Size(82, 32);
+            this.PESO.TabIndex = 3;
+            this.PESO.Text = "PESO";
+            this.PESO.Click += new System.EventHandler(this.PESO_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.flowLayoutPanel1.Controls.Add(this.PESO);
+            this.flowLayoutPanel1.Controls.Add(this.textBox3);
+            this.flowLayoutPanel1.Controls.Add(this.ALTURA);
+            this.flowLayoutPanel1.Controls.Add(this.textBox1);
+            this.flowLayoutPanel1.Controls.Add(this.RESULTADO);
+            this.flowLayoutPanel1.Controls.Add(this.textBox2);
+            this.flowLayoutPanel1.Controls.Add(this.CALCULAR);
+            this.flowLayoutPanel1.Controls.Add(this.LIMPAR);
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(31, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(353, 426);
+            this.flowLayoutPanel1.TabIndex = 11;
+            this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(381, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(311, 271);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1065, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "CalculadoraIMC";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label ALTURA;
+        private System.Windows.Forms.Label RESULTADO;
+        private System.Windows.Forms.Button CALCULAR;
+        private System.Windows.Forms.Button LIMPAR;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label PESO;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraIMC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CALCULAR_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 1. Pega o que está escrito nas caixas (texto)
+                string textoPeso = textBox3.Text;
+                string textoAltura = textBox1.Text;
+
+                // 2. Transforma o texto em números (double)
+                double p = double.Parse(textoPeso);
+                double a = double.Parse(textoAltura);
+
+                // 3. Faz a conta do IMC
+                double imc = p / (a * a);
+
+                // 4. Mostra o resultado na tela (virando texto de novo)
+                textBox2.Text = imc.ToString("F2");
+
+                string classificacao = "";
+
+                // Classificação do IMC
+                if (imc < 18.5)
+                {
+                    classificacao = "Peso Baixo";
+                }
+                else if (imc >= 18.5 && imc <= 24.9)
+                {
+                    classificacao = "Peso Normal";
+                }
+                else if (imc >= 25 && imc <= 29.9)
+                {
+                    classificacao = "Sobrepeso";
+                }
+                else if (imc >= 30 && imc <= 34.9)
+                {
+                    classificacao = "Obesidade Grau 1";
+                }
+                else if (imc >= 35 && imc <= 39.9)
+                {
+                    classificacao = "Obesidade Grau 2";
+                }
+                else
+                {
+                    classificacao = "Obesidade Grau 3";
+                }
+
+                // Mostrar resultado + classificação
+                textBox2.Text = imc.ToString("F2") + " - " + classificacao;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro técnico: " + ex.Message);
+            }
+
+} 
+        private void LIMPAR_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+
+            textBox3.Focus();
+        }
+
+        private void PESO_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
